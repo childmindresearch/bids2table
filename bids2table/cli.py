e3a55fa3ba9b8398c257669d302c1ef77ed078ff
Florian Rupprecht <floruppr@gmail.com>
2023-05-24 10:59:18 -0400

    Renaming; Cleanup

--- bids2table/cli.py
+++ bids2table/cli.py
@@ -10,47 +10,45 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-i", "--input",
-        type=Path,
-        help="Path to BIDS dataset",
-        required=True
+        "-i", "--input", type=Path, help="Path to BIDS dataset.", required=True
     )
     parser.add_argument(
-        "-o", "--output",
+        "-o",
+        "--output",
         type=Path,
         help="Path to output parquet dataset directory",
-        required=True
+        required=True,
     )
     parser.add_argument(
-        "-u", "--update",
-        help="update dataset incrementally with only new or changed files.",
-        action="store_true"
+        "-u",
+        "--incremental",
+        help="Update dataset incrementally with only new or changed files.",
+        action="store_true",
     )
     parser.add_argument(
-        "-c", "--cores",
+        "-w",
+        "--workers",
         type=int,
-        help="number of parallel processes. If `None` or 1, run in the main"
-             "process. Setting to -1 runs in `os.cpu_count()` processes.",
-        default=None
+        help="Number of parallel processes. If `None` or 1, run in the main"
+        "process. Setting to -1 runs in `os.cpu_count()` processes.",
+        default=None,
     )
     parser.add_argument(
-        "-s", "--suppress_warnings",
-        help="suppress warnings",
-        action="store_true"
+        "-v", "--verbose", help="Verbose logging.", action="store_true"
     )
 
     args = parser.parse_args()
 
-    if args.suppress_warnings:
+    if not args.verbose:
         elbow.utils.setup_logging("ERROR")
 
     load_bids_parquet(
         path=args.input,
         where=args.output,
-        incremental=args.update,
-        workers=args.cores
+        incremental=args.incremental,
+        workers=args.workers,
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
