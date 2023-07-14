d0f1ba92353ca42c0d76144bc1e981c8e82f5422
Connor Lane <connor.lane858@gmail.com>
2023-07-14 14:20:01 -0400

    Fix handling of null entities
    Previous way of detecting null entities missed `np.nan`. Switch to using
    `pandas.isna()`.

--- bids2table/extractors/entities.py
+++ bids2table/extractors/entities.py
@@ -4,6 +4,7 @@ from functools import lru_cache
 from pathlib import Path
 from typing import Any, Dict, Iterable, Optional, Union
 
+import pandas as pd
 from elbow.typing import StrOrPath
 from typing_extensions import get_args, get_origin
 
@@ -115,7 +116,7 @@ class BIDSEntities:
             extra_entities[k] = v
 
         for key, val in entities.items():
-            if _is_na(val):
+            if pd.isna(val):
                 continue
 
             if key in fields_map:
@@ -183,7 +184,7 @@ class BIDSEntities:
         name = "_".join(
             f"{k}-{v}" if len(str(v)) > 0 else str(k)
             for k, v in data.items()
-            if k not in special and not _is_na(v)
+            if k not in special and not pd.isna(v)
         )
         if self.suffix:
             name += f"_{self.suffix}"
@@ -212,10 +213,6 @@ class BIDSEntities:
         if kwargs:
             data.update(kwargs)
         return BIDSEntities.from_dict(data)
-
-
-def _is_na(value: Any) -> bool:
-    return value is None or value == float("nan")
 
 
 def _get_type(alias: Any) -> type:
