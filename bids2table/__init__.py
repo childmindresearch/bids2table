b7b165838a1d631c51e370f7c088318af4b7c6da
Connor Lane <connor.lane858@gmail.com>
2023-07-17 10:51:40 -0400

    Add version (#11)

--- bids2table/__init__.py
+++ bids2table/__init__.py
@@ -3,3 +3,4 @@ Efficiently index large-scale BIDS datasets and derivatives
 """
 
 from ._bids2table import bids2table, load_index  # noqa
+from ._version import __version__, __version_tuple__  # noqa
