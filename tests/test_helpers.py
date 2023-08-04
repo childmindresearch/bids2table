c97e550bb73b5c347dabf2d29d675acef3275e0d
Connor Lane <connor.lane858@gmail.com>
2023-08-04 12:55:07 -0400

    Add test for flat <-> multi column conversion

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,7 +3,11 @@ from typing import Any, Dict, Optional
 import pandas as pd
 import pytest
 
-from bids2table.helpers import join_bids_path
+from bids2table.helpers import (
+    flat_to_multi_columns,
+    join_bids_path,
+    multi_to_flat_columns,
+)
 
 
 @pytest.mark.parametrize(
@@ -87,6 +91,25 @@ def test_join_bids_path(
 ):
     path = join_bids_path(row, prefix=prefix, valid_only=valid_only)
     assert str(path) == expected
+
+
+@pytest.mark.parametrize("sep", ["__", "."])
+def test_flat_to_multi_columns(sep: str):
+    df = pd.DataFrame(
+        {
+            f"A{sep}a": [1, 2, 3],
+            f"A{sep}b": ["a", "b", "c"],
+            f"B{sep}a": [4, 5, 6],
+            f"B{sep}b": ["d", "e", "f"],
+        }
+    )
+    multi_index = pd.MultiIndex.from_product([["A", "B"], ["a", "b"]])
+
+    df_multi = flat_to_multi_columns(df, sep=sep)
+    assert df_multi.columns.equals(multi_index)
+
+    df_flat = multi_to_flat_columns(df_multi, sep=sep)
+    assert df_flat.equals(df)
 
 
 if __name__ == "__main__":
