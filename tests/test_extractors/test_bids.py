a99052eeaa3d655c25e4e39cbf11680de0de1361
Connor Lane <connor.lane858@gmail.com>
2024-07-09 16:50:53 -0700

    Add test for `is_bids_file` with one failing case
    Test includes failing case for the 'ome.zarr' directories which are in
    fact data "files".
    
    Nb, I don't really like this pattern. IMO these directories should be
    e.g. tar archives. It doesn't seem worth breaking the rule that data
    files are files.

---
+++ tests/test_extractors/test_bids.py
@@ -0,0 +1,19 @@
+import pytest
+
+from bids2table.extractors.bids import is_bids_file
+
+
+@pytest.mark.parametrize(
+    "path,expected",
+    [
+        ("sub-A01/ses-1/anat/sub-A01_ses-1_T1w.nii.gz", True),
+        ("sub-A01/ses-1/anat/sub-A01_ses-1_T1w.json", False),
+        ("sub-A01/ses-1/anat/sub-A01_ses-1_keyvalue.json", True),
+        ("sub-A01/ses-1/anat/sub-A01_ses-1_white.surf.json", True),
+        ("sub-A01/ses-1/micr/sub-A01_ses-1_sample-A_SPIM.ome.zarr", True),
+    ],
+)
+def test_is_bids_file(path, expected, bids_dataset):
+    ds_dir, _, _ = bids_dataset
+    path = ds_dir / path
+    assert is_bids_file(path) == expected
