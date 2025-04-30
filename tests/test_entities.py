d292d2bf5969a7c6f57efc771fd9ec2a4998fdc0
Connor Lane <connor.lane858@gmail.com>
2025-04-30 17:01:03 -0400

    Add entities utilities
    - BIDS schema loaded from `bidschematools` following previous impl (thx
      @nx10).
    - Add Arrow schema construction
    - Otherwise, trying to make much simpler. No dataclass complexity.

---
+++ tests/test_entities.py
@@ -0,0 +1,100 @@
+from typing import Any, NamedTuple
+
+import pytest
+from pytest import FixtureRequest
+
+from bids2table._entities import (
+    format_path,
+    parse_bids_entities,
+    validate_bids_entities,
+)
+
+
+class ExampleCase(NamedTuple):
+    path: str
+    valid_entities: dict[str, Any]
+    extra_entities: dict[str, Any]
+
+
+EXAMPLES = [
+    ExampleCase(
+        "dataset/sub-A01/ses-B02/func/sub-A01_ses-B02_task-rest_run-1_bold.nii.gz",
+        {
+            "sub": "A01",
+            "ses": "B02",
+            "task": "rest",
+            "run": 1,
+            "datatype": "func",
+            "suffix": "bold",
+            "ext": ".nii.gz",
+        },
+        {},
+    ),
+    ExampleCase(
+        "sub-A01/func/sub-A01_task-rest_run-1_bold.nii.gz",
+        {
+            "sub": "A01",
+            "task": "rest",
+            "run": 1,
+            "datatype": "func",
+            "suffix": "bold",
+            "ext": ".nii.gz",
+        },
+        {},
+    ),
+    ExampleCase(
+        "sub-A01_extraKey-true_extraKeyNoValue_bold.nii",
+        {"sub": "A01", "suffix": "bold", "ext": ".nii"},
+        {"extraKey": "true"},
+    ),
+    ExampleCase(
+        "sub-A01_noExt",
+        {"sub": "A01", "suffix": "noExt"},
+        {},
+    ),
+    ExampleCase(
+        "sub-A01_no-suffix",
+        {"sub": "A01"},
+        {"no": "suffix"},
+    ),
+]
+
+
+@pytest.fixture(params=EXAMPLES)
+def bids_example(request: FixtureRequest) -> ExampleCase:
+    return request.param
+
+
+def test_parse_validate_bids_entities(bids_example: ExampleCase):
+    path, expected_valid_entities, expected_extra_entities = bids_example
+    entities = parse_bids_entities(path)
+    valid_entities, extra_entities = validate_bids_entities(entities)
+    assert valid_entities == expected_valid_entities
+    assert extra_entities == expected_extra_entities
+
+
+@pytest.mark.parametrize(
+    "path,msg",
+    [
+        ("sub-A01_run-abc_bold.nii.gz", "type"),  # Invalid run type
+        ("sub-A01_part-phasee_bold.nii.gz", "allowed"),  # Not in allowed values, typo
+    ],
+)
+def test_validate_raises(path: str, msg: str):
+    entities = parse_bids_entities(path)
+    with pytest.raises(ValueError, match=msg):
+        validate_bids_entities(entities)
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "sub-A01/func/sub-A01_run-1_bold.nii.gz",
+        "sub-A01/ses-1/func/sub-A01_ses-1_run-1_bold.nii.gz",
+    ],
+)
+def test_format_path(path: str):
+    entities = parse_bids_entities(path)
+    valid_entities, _ = validate_bids_entities(entities)
+    path2 = format_path(valid_entities)
+    assert path == str(path2)
